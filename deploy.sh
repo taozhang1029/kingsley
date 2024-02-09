b07eef51cad974eba6be1496796b400cf0820e91
kingsley <taozhang1029@qq.com>
2024-02-09 18:09:37 +0800

    github pages

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,8 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:taozhang1029.github.io.git develop
+git push -f https://github.com/taozhang1029/blog.git origin HEAD:gh-pages
+
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
